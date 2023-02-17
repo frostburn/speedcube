46aa7ca8d672e734cebe7fe00b11e2d3f4695190
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-02-17 08:01:24 +0200

    Make the 3x3x3 solver more resusable

--- main.c
+++ main.c
@@ -580,20 +580,43 @@ void solve_edges() {
   free_goalsphere(&edge_sphere);
 }
 
-void solve_3x3x3() {
+typedef struct {
+  Nibblebase first;
+  Nibblebase last;
+  Nibblebase corners;
+  IDAstar ida;
+} Solver;
+
+Solver GLOBAL_SOLVER;
+
+unsigned char global_estimator(LocDirCube *ldc) {
+  unsigned char first_depth = get_nibble(&GLOBAL_SOLVER.first, (*GLOBAL_SOLVER.first.index_func)(ldc));
+  unsigned char last_depth = get_nibble(&GLOBAL_SOLVER.last, (*GLOBAL_SOLVER.last.index_func)(ldc));
+  unsigned char corners_depth = get_nibble(&GLOBAL_SOLVER.corners, (*GLOBAL_SOLVER.corners.index_func)(ldc));
+
+  if (first_depth > last_depth && first_depth > corners_depth) {
+    return first_depth;
+  }
+  if (corners_depth > last_depth) {
+    return corners_depth;
+  }
+  return last_depth;
+}
+
+void prepare_global_solver() {
   FILE *fptr;
   size_t num_read;
   size_t tablebase_size;
 
   printf("Loading tablebase for first 7 edges.\n");
-  Nibblebase first = init_nibblebase(LOCDIR_FIRST_7_EDGE_INDEX_SPACE, &locdir_first_7_edge_index);
+  GLOBAL_SOLVER.first = init_nibblebase(LOCDIR_FIRST_7_EDGE_INDEX_SPACE, &locdir_first_7_edge_index);
   fptr = fopen("./tables/first_7_edges.bin", "rb");
   if (fptr == NULL) {
     fprintf(stderr, "Failed to open file.\n");
     exit(EXIT_FAILURE);
   }
   tablebase_size = (LOCDIR_FIRST_7_EDGE_INDEX_SPACE + 1)/2;
-  num_read = fread(first.octets, sizeof(unsigned char), tablebase_size, fptr);
+  num_read = fread(GLOBAL_SOLVER.first.octets, sizeof(unsigned char), tablebase_size, fptr);
   if (num_read != tablebase_size) {
     fprintf(stderr, "Failed to load data. Only %zu of %zu read.\n", num_read, tablebase_size);
     exit(EXIT_FAILURE);
@@ -601,14 +624,14 @@ void solve_3x3x3() {
   fclose(fptr);
 
   printf("Loading tablebase for last 7 edges.\n");
-  Nibblebase last = init_nibblebase(LOCDIR_LAST_7_EDGE_INDEX_SPACE, &locdir_last_7_edge_index);
+  GLOBAL_SOLVER.last = init_nibblebase(LOCDIR_LAST_7_EDGE_INDEX_SPACE, &locdir_last_7_edge_index);
   fptr = fopen("./tables/last_7_edges.bin", "rb");
   if (fptr == NULL) {
     fprintf(stderr, "Failed to open file.\n");
     exit(EXIT_FAILURE);
   }
   tablebase_size = (LOCDIR_LAST_7_EDGE_INDEX_SPACE + 1)/2;
-  num_read = fread(last.octets, sizeof(unsigned char), tablebase_size, fptr);
+  num_read = fread(GLOBAL_SOLVER.last.octets, sizeof(unsigned char), tablebase_size, fptr);
   if (num_read != tablebase_size) {
     fprintf(stderr, "Failed to load data. Only %zu of %zu read.\n", num_read, tablebase_size);
     exit(EXIT_FAILURE);
@@ -616,56 +639,51 @@ void solve_3x3x3() {
   fclose(fptr);
 
   printf("Loading tablebase for the corners.\n");
-  Nibblebase corners = init_nibblebase(LOCDIR_CORNER_INDEX_SPACE, &locdir_corner_index);
+  GLOBAL_SOLVER.corners = init_nibblebase(LOCDIR_CORNER_INDEX_SPACE, &locdir_corner_index);
   fptr = fopen("./tables/corners.bin", "rb");
   if (fptr == NULL) {
     fprintf(stderr, "Failed to open file.\n");
     exit(EXIT_FAILURE);
   }
   tablebase_size = (LOCDIR_CORNER_INDEX_SPACE + 1)/2;
-  num_read = fread(corners.octets, sizeof(unsigned char), tablebase_size, fptr);
+  num_read = fread(GLOBAL_SOLVER.corners.octets, sizeof(unsigned char), tablebase_size, fptr);
   if (num_read != tablebase_size) {
     fprintf(stderr, "Failed to load data. Only %zu of %zu read.\n", num_read, tablebase_size);
     exit(EXIT_FAILURE);
   }
   fclose(fptr);
 
-  unsigned char estimator(LocDirCube *ldc) {
-    unsigned char first_depth = get_nibble(&first, (*first.index_func)(ldc));
-    unsigned char last_depth = get_nibble(&last, (*last.index_func)(ldc));
-    unsigned char corners_depth = get_nibble(&corners, (*corners.index_func)(ldc));
+  GLOBAL_SOLVER.ida.is_solved = locdir_solved;
+  GLOBAL_SOLVER.ida.estimator = global_estimator;
+}
 
-    if (first_depth > last_depth && first_depth > corners_depth) {
-      return first_depth;
-    }
-    if (corners_depth > last_depth) {
-      return corners_depth;
-    }
-    return last_depth;
-  }
+void free_global_solver() {
+  free_nibblebase(&GLOBAL_SOLVER.first);
+  free_nibblebase(&GLOBAL_SOLVER.last);
+  free_nibblebase(&GLOBAL_SOLVER.corners);
+}
 
-  IDAstar ida;
-  ida.is_solved = locdir_solved;
-  ida.estimator = estimator;
+void solve_3x3x3() {
+  prepare_global_solver();
 
   LocDirCube ldc;
   locdir_reset(&ldc);
-
 
   printf("Solving a few scrambles...\n");
   for (size_t j = 0; j < 5; j++) {
     locdir_scramble(&ldc);
 
-    ida_star_solve(&ida, &ldc);
+    ida_star_solve(&GLOBAL_SOLVER.ida, &ldc);
 
-    printf("Found a solution in %zu moves:\n", ida.path_length - 1);
+    printf("Found a solution in %zu moves:\n", GLOBAL_SOLVER.ida.path_length - 1);
 
-    for (size_t i = 0; i < ida.path_length; ++i) {
-      Cube cube = to_cube(ida.path + i);
+    for (size_t i = 0; i < GLOBAL_SOLVER.ida.path_length; ++i) {
+      Cube cube = to_cube(GLOBAL_SOLVER.ida.path + i);
       render(&cube);
     }
   }
 
+  /*
   printf("Collecting statistics...\n");
 
   clock_t start = clock();
@@ -694,11 +712,11 @@ void solve_3x3x3() {
   printf("Minimum number of moves in a solution = %zu\n", min_moves);
   printf("Average number of moves in a solution = %g\n", ((double)total_moves) / total_solves);
   printf("Maximum number of moves in a solution = %zu\n", max_moves);
+  */
 
-  free_nibblebase(&first);
-  free_nibblebase(&last);
-  free_nibblebase(&corners);
+  free_global_solver();
 }
+
 
 void pll_solutions() {
   FILE *fptr;
@@ -936,9 +954,9 @@ int main() {
 
   // solve_edges();
 
-  // solve_3x3x3();
+  solve_3x3x3();
 
-  pll_solutions();
+  // pll_solutions();
 
   // oll_solutions();
 
