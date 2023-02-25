b1b0c0bf9c0ae78ec3ce300451e1604107c7b372
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-02-25 21:39:06 +0200

    Implement parallel solving using OpenMP

--- ida_star.c
+++ ida_star.c
@@ -1,3 +1,5 @@
+#define LOG_IDA_STAR_PROGRESS 0
+
 typedef struct {
   LocDirCube path[SEQUENCE_MAX_LENGTH];
   size_t path_length;
@@ -6,6 +8,7 @@ typedef struct {
 } IDAstar;
 
 const unsigned char FOUND = 254;
+const unsigned char SKIP = 253;
 
 unsigned char ida_star_search(IDAstar *ida, unsigned char so_far, unsigned char bound) {
   LocDirCube *ldc = ida->path + (ida->path_length - 1);
@@ -17,7 +20,7 @@ unsigned char ida_star_search(IDAstar *ida, unsigned char so_far, unsigned char 
   if (to_go == 0 && (*ida->is_solved)(ldc)) {
     return FOUND;
   }
-  unsigned char min = 255;
+  unsigned char min = UNKNOWN;
   for (size_t i = 0; i < NUM_STABLE_MOVES; ++i) {
     ida->path[ida->path_length] = *ldc;
     locdir_apply_stable(ida->path + ida->path_length, STABLE_MOVES[i]);
@@ -57,12 +60,136 @@ void ida_star_solve(IDAstar *ida, LocDirCube *ldc, unsigned char lower_bound) {
   }
 
   for (;;) {
+    #if LOG_IDA_STAR_PROGRESS
+    printf("IDA* bound = %d\n", bound);
+    #endif
     unsigned char search_result = ida_star_search(ida, 0, bound);
     if (search_result == FOUND) {
       // Solution is stored in ida->path.
       return;
     }
     bound = search_result;
+  }
+}
+
+void ida_star_solve_parallel(IDAstar *ida, LocDirCube *ldc, unsigned char lower_bound) {
+  ida->path[0] = *ldc;
+  ida->path_length = 1;
+  // Check if already solved
+  if ((*ida->is_solved)(ldc)) {
+    return;
+  }
+
+  unsigned char bound = (*ida->estimator)(ida->path);
+
+  if (lower_bound > bound) {
+    bound = lower_bound;
+  }
+
+  IDAstar ida_team[NUM_STABLE_MOVES * NUM_STABLE_MOVES];
+  for (int i = 0; i < NUM_STABLE_MOVES; ++i) {
+    for (int j = 0; j < NUM_STABLE_MOVES; ++j) {
+      int idx = i * NUM_STABLE_MOVES + j;
+      ida_team[idx].path[0] = *ldc;
+      ida_team[idx].path[1] = ida_team[idx].path[0];
+      locdir_apply_stable(ida_team[idx].path + 1, STABLE_MOVES[i]);
+      ida_team[idx].path[2] = ida_team[idx].path[1];
+      locdir_apply_stable(ida_team[idx].path + 2, STABLE_MOVES[j]);
+      ida_team[idx].path_length = 3;
+
+      ida_team[idx].is_solved = ida->is_solved;
+      ida_team[idx].estimator = ida->estimator;
+    }
+  }
+
+  // Check if the first move already solves
+  for (int i = 0; i < NUM_STABLE_MOVES * NUM_STABLE_MOVES; ++i) {
+    if ((*ida->is_solved)(ida_team[i].path + 1)) {
+      ida->path[0] = ida_team[i].path[0];
+      ida->path[1] = ida_team[i].path[1];
+      ida->path_length = 2;
+      return;
+    }
+  }
+  // Check if the second move already solves
+  for (int i = 0; i < NUM_STABLE_MOVES * NUM_STABLE_MOVES; ++i) {
+    if ((*ida->is_solved)(ida_team[i].path + 2)) {
+      ida->path[0] = ida_team[i].path[0];
+      ida->path[1] = ida_team[i].path[1];
+      ida->path[2] = ida_team[i].path[2];
+      ida->path_length = 3;
+      return;
+    }
+  }
+
+  unsigned char team_results[NUM_STABLE_MOVES * NUM_STABLE_MOVES] = {0};
+  size_t num_members = 0;
+  for (int i = 0; i < NUM_STABLE_MOVES * NUM_STABLE_MOVES; ++i) {
+    // Skip backtracking
+    if (
+      locdir_equals(ida_team[i].path, ida_team[i].path + 1) ||
+      locdir_equals(ida_team[i].path, ida_team[i].path + 2) ||
+      locdir_equals(ida_team[i].path + 1, ida_team[i].path + 2)
+    ) {
+      // printf("%d = (%d, %d) backtracks\n", i, i % NUM_STABLE_MOVES, i / NUM_STABLE_MOVES);
+      team_results[i] = SKIP;
+      continue;
+    }
+    // Skip duplicates
+    for (int j = 0; j < i; ++j) {
+      if (team_results[j] == SKIP) {
+        continue;
+      }
+      if (
+        locdir_equals(ida_team[i].path + 2, ida_team[j].path + 1) ||
+        locdir_equals(ida_team[i].path + 2, ida_team[j].path + 2)
+      ) {
+        // printf("%d = (%d, %d) is a duplicate\n", i, i % NUM_STABLE_MOVES, i / NUM_STABLE_MOVES);
+        team_results[i] = SKIP;
+        break;
+      }
+    }
+    if (team_results[i] != SKIP) {
+      num_members++;
+    }
+  }
+
+  #if LOG_IDA_STAR_PROGRESS
+  printf("%zu members in IDA* team\n", num_members);
+  #endif
+
+
+  for (;;) {
+    #if LOG_IDA_STAR_PROGRESS
+    printf("IDA* bound = %d\n", bound);
+    #endif
+    bool found = false;
+    #pragma omp parallel for schedule(dynamic)
+    for (int i = 0; i < NUM_STABLE_MOVES * NUM_STABLE_MOVES; ++i) {
+      if (team_results[i] == SKIP || found) {
+        continue;
+      }
+      team_results[i] = ida_star_search(ida_team + i, 2, bound);
+      if (team_results[i] == FOUND) {
+        found = true;
+      }
+    }
+
+    bound = UNKNOWN;
+    for (int i = 0; i < NUM_STABLE_MOVES * NUM_STABLE_MOVES; ++i) {
+      if (team_results[i] == SKIP) {
+        continue;
+      }
+      if (team_results[i] == FOUND) {
+        // Store result in ida->path
+        ida->path_length = ida_team[i].path_length;
+        for (int j = 0; j < ida->path_length; ++j) {
+          ida->path[j] = ida_team[i].path[j];
+        }
+        return;
+      }
+      bound = team_results[i] < bound ? team_results[i] : bound;
+    }
   }
 }
 
